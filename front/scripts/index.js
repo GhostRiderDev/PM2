f7fadedc5058c7773a8bd4dd6d2c4fa9c069b43f
cos4h <olvadis2004@gmail.com>
2024-02-05 19:19:48 -0500

    I make some UI

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,15 @@
-console.log(tempData);
+function addCardsToSlider() {
+  const slider = document.getElementsByClassName("carrusel")[0];
+  tempData.forEach((movie) => {
+    slider.appendChild(createCard(movie));
+  });
+}
+
+function createCard(movie) {
+  const img = document.createElement("img");
+  img.src = movie.poster;
+  img.classList = "movieCard";
+  return img;
+}
+
+addCardsToSlider();
