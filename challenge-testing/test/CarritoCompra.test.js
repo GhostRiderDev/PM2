2e4aa61bb9a1f80478126c49268cafda41c6a961
cos4h <olvadis2004@gmail.com>
2024-02-12 22:28:50 -0500

    All test was passed

--- challenge-testing/test/CarritoCompra.test.js
+++ challenge-testing/test/CarritoCompra.test.js
@@ -41,8 +41,20 @@ describe("agregarProducto method", () => {
   });
 
   it("If object is not sent, throw error", () => {
-    expect(() => carrito.agregarProducto({})).toThrowError(
+    expect(() => carrito.agregarProducto({})).toThrow(
       new Error("Product empty")
+    );
+  });
+
+  it("If properties of object send is not valid, throw error", () => {
+    expect(() =>
+      carrito
+        .agregarProducto({
+          nombre: "Arroz",
+          precio: 6000,
+          cantidad: 5,
+        })
+        .toThrowError(new Error("invalid product"))
     );
   });
 
@@ -54,5 +66,84 @@ describe("agregarProducto method", () => {
     };
     carrito.agregarProducto(product);
     expect(carrito.compras).toContainEqual(product);
+  });
+});
+
+describe("calcularTotal method", () => {
+  let carrito;
+
+  beforeEach(() => {
+    carrito = new CarritoCompra([
+      {
+        nombre: "Leche",
+        precio: 5000,
+        cantidad: 3,
+      },
+      {
+        nombre: "Arroz",
+        precio: 6000,
+        cantidad: 5,
+      },
+    ]);
+  });
+
+  it("calcularTotal must be is defined", () => {
+    expect(carrito.calcularTotal).toBeDefined();
+  });
+  it("calcularTotal must be type function", () => {
+    expect(typeof carrito.calcularTotal).toBe("function");
+  });
+  it("calcularTotal must be return a number", () => {
+    expect(typeof carrito.calcularTotal()).toBe("number");
+  });
+  it("calcularTotal must bereturn a positive number", () => {
+    expect(carrito.calcularTotal()).toBeGreaterThan(-1);
+  });
+  it("calculartotal must be return 45000", () => {
+    expect(carrito.calcularTotal()).toBe(45000);
+  });
+  it("if data is empty must be return 0", () => {
+    carrito = new CarritoCompra([]);
+    expect(carrito.calcularTotal()).toBe(0);
+  });
+});
+
+describe("aplicarDescuento method", () => {
+  let carrito;
+
+  beforeEach(() => {
+    carrito = new CarritoCompra([
+      {
+        nombre: "Leche",
+        precio: 5000,
+        cantidad: 3,
+      },
+      {
+        nombre: "Arroz",
+        precio: 6000,
+        cantidad: 5,
+      },
+    ]);
+  });
+
+  it("aplicarDescuento must be is defined", () => {
+    expect(carrito.calcularTotal).toBeDefined();
+  });
+  it("aplicarDescuento must be type function", () => {
+    expect(typeof carrito.calcularTotal).toBe("function");
+  });
+  it("if not send percentage no apply discount", () => {
+    expect(carrito.aplicarDescuento()).toBe(45000);
+  });
+  it("percentage is in % not decimal so apply in %", () => {
+    expect(carrito.aplicarDescuento(0.5)).toBe(45000 - (0.5 / 100) * 45000);
+  });
+  it("if not send type number throw error", () => {
+    expect(() => carrito.aplicarDescuento("15%")).toThrow(
+      new Error("Invalid type")
+    );
+  });
+  it("if data is success apply discount", () => {
+    expect(carrito.aplicarDescuento(50)).toBe(45000 / 2);
   });
 });
--- challenge-testing/test/index.js
+++ challenge-testing/test/index.js
@@ -1,15 +1,28 @@
 class CarritoCompra {
-  constructor() {
+  constructor(initialCompras = []) {
     // Aqui va el codigo
+    this.compras = initialCompras;
   }
-  agregarProducto() {
+  agregarProducto(product = {}) {
     // Aqui va el codigo
+    if (!product || Object.keys(product).length === 0) {
+      throw new Error("Product empty");
+    }
+    this.compras.push(product);
+    return this.compras.length;
   }
   calcularTotal() {
-    // Aqui va el codigo
+    return this.compras.reduce(
+      (acc, item) => acc + item.cantidad * item.precio,
+      0
+    );
   }
-  aplicarDescuento() {
+  aplicarDescuento(percentage = 0) {
     // Aqui va el codigo
+    if (typeof percentage !== "number") {
+      throw new Error("Invalid type");
+    }
+    return this.calcularTotal() * ((100 - percentage) / 100);
   }
 }
 
